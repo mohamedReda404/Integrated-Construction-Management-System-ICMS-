41e8fb3323f629f505e2793c157a2b64f03f7233
Awad Hassan <awadhassan638@yahoo.com>
2025-12-13 15:44:18 +0200

    add all Controllers empty

---
+++ backend/ICMS/Integrated-Construction-Management-System-ICMS/Controllers/BoqConditionEngController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Integrated_Construction_Management_System_ICMS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BoqConditionEngController : ControllerBase
+    {
+    }
+}
