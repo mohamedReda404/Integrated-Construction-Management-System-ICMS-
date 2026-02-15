6203753e96d1bba5a810aa503452730c05df5292
mohamedReda404 <mohamedredaodah89@gmail.com>
2026-02-15 23:33:03 +0200

    Add Dto's ProjectContract

---
+++ backend/ICMS/Integrated-Construction-Management-System-ICMS/Contracts/Requests/ProjectContractRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace Integrated_Construction_Management_System_ICMS.Contracts.Requests
+{
+    public record ProjectContractRequest
+    (
+        DateTime ContractDate,
+        string ContractDetails,
+        string EndContractIfs,
+        string ClientCondition,
+        string ClientSignature,
+        string ManagerSignature,
+        string ManagerCondition
+        );
+}
---
+++ backend/ICMS/Integrated-Construction-Management-System-ICMS/Contracts/Responces/ProjectContractResponce.cs
@@ -0,0 +1,14 @@
+﻿namespace Integrated_Construction_Management_System_ICMS.Contracts.Responces
+{
+    public record ProjectContractResponce
+    (
+        int ProjectContractId,
+        DateTime ContractDate,
+        string ContractDetails,
+        string EndContractIfs,
+        string ClientCondition,
+        string ClientSignature,
+        string ManagerSignature,
+        string ManagerCondition
+        );
+}
