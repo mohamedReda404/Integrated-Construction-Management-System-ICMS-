5ed7e96a85a985f3db486927b67b2448e05c7760
mohamedReda404 <mohamedredaodah89@gmail.com>
2026-02-19 06:24:25 +0200

    GenerateJwtToken IN JwtProvider Services

---
+++ backend/ICMS/Integrated-Construction-Management-System-ICMS/Authentication/IJwtProvider.cs
@@ -0,0 +1,7 @@
+﻿namespace Integrated_Construction_Management_System_ICMS.Authentication
+{
+    public interface IJwtProvider
+    {
+        (string token, int expirsIn) GenerateToken(ApplicationUser user);
+    }
+}
